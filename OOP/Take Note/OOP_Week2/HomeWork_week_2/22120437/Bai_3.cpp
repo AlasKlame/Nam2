90d238a63cb06e1b57dd8314fa6f3c822b1a01dd
Vuco <vuledinhhoang@gmail.com>
2024-03-09 12:08:47 +0700

    Homework

---
+++ OOP/Take Note/OOP_Week2/HomeWork_week_2/22120437/Bai_3.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+
+using namespace std;
+
+class DynamicArray{
+    private:
+        int size;
+        int *arr;
+    public:
+
+        DynamicArray(int size){
+            this->size = size;;
+            arr = new int[size];
+        }
+
+        DynamicArray(){
+            delete[] arr;
+        }
+
+        void input(){
+            for(int i=0; i<size; i++){
+                cin >> arr[i];
+            }
+        }
+
+        void output(){
+            for(int i=0; i<size; i++){
+                cout << arr[i] << " ";
+            }
+        }
+
+        void sort(){
+
+            for(int i=0; i< size - 1; i++){
+                for(int j = 0; j < size - i - 1; j++){
+                    if(arr[j] > arr[j+1]){
+                        int tmp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = tmp;
+                    }
+                }
+            }
+        }
+
+        int secondElement(){
+            return arr[size-2];
+        }
+};
+
+
+
+int main(){
+    int n;
+    cin >> n;
+    DynamicArray array(n);
+    array.input();
+    array.sort();
+    array.secondElement();
+    array.output();
+    cout << "Phan tu lon thu 2 la \n" << array.secondElement() << endl;
+    DynamicArray();
+    return 0;
+
+
+}
+
+    
+
