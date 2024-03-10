b84000be4f20dfad9a4fb6cf76b3120809312f77
Vuco <vuledinhhoang@gmail.com>
2024-03-11 06:03:53 +0700

    Homework OOP(Done)

--- OOP/Take Note/OOP_Week2/HomeWork_week_2/22120437/bai_4.cpp
+++ OOP/Take Note/OOP_Week2/HomeWork_week_2/22120437/bai_4.cpp
@@ -43,13 +43,17 @@ class Stack{    //LIFO
             }
             return arr[top--];
         }
-
+        
+        
         int peek(){
             if(isEmpty()){
                 cout << "Stack is empty" << endl;
                 return -1;
             }
             return arr[top];
+        }
+        int updateSize(){
+            return top + 1;
         }
         void print(){
             for(int i = 0; i <= top; i++){
@@ -61,20 +65,65 @@ class Stack{    //LIFO
 };
 
 int main(){
-    int n;
-    cin >> n;
-    Stack stack(n);
-    while(stack.isFull() == false){
-        int element;
-        cin >> element;
-        stack.push(element);
+    int size;
+    cout << "Nhap kich thuoc stack: ";
+    cin >> size;
+    Stack stack(size);
+    {
+        int choice;
+        do{
+            cout << "1. Them phan tu vao stack" << endl;
+            cout << "2. Xoa phan tu khoi stack" << endl;
+            cout << "3. Lay phan tu o dau stack" << endl;
+            cout << "4. Kiem tra stack rong" << endl;
+            cout << "5. Kiem tra stack day" << endl;
+            cout << "6. Lay kich thuoc stack" << endl;
+            cout << "7. In stack" << endl;
+            cout << "8. Thoat" << endl;
+            cout << "Nhap lua chon: ";
+            cin >> choice;
+            switch(choice){
+                case 1:
+                    int element;
+                    cout << "Nhap phan tu can them: ";
+                    cin >> element;
+                    stack.push(element);
+                    break;
+                case 2:
+                    stack.pop();
+                    break;
+                case 3:
+                    cout << "Phan tu o dau stack: " << stack.peek() << endl;
+                    break;
+                case 4:
+                    if(stack.isEmpty()){
+                        cout << "Stack rong" << endl;
+                    }else{
+                        cout << "Stack khong rong" << endl;
+                    }
+                    break;
+                case 5:
+                    if(stack.isFull()){
+                        cout << "Stack day" << endl;
+                    }else{
+                        cout << "Stack chua day" << endl;
+                    }
+                    break;
+                case 6:
+                    cout << "Kich thuoc stack: " << stack.updateSize() << endl;
+                    break;
+                case 7:
+                    stack.print();
+                    cout << endl;
+                    break;
+                case 8:
+                    cout << "Tam biet" << endl;
+                    break;
+                default:
+                    cout << "Lua chon khong hop le" << endl;
+                    break;
+            }
+        }while(choice != 8);
     }
-    cout << "Stack sau khi push" << endl;
-    stack.print();
-    cout << endl;
-    cout << "xem phan tu dau tien " << stack.peek() << endl;
-    cout << "Lay phan tu dau tien va xoa ra khoi stack: " <<  stack.pop() << endl;
-    cout << "stack sau khi lay phan tu dau tien: ";
-    stack.print();
     return 0;
 }
--- OOP/Take Note/OOP_Week2/HomeWork_week_2/22120437/bai_5.cpp
+++ OOP/Take Note/OOP_Week2/HomeWork_week_2/22120437/bai_5.cpp
@@ -2,101 +2,186 @@
 
 using namespace std;
 
-class Queue{
-    private:
-        int size;
-        int front;
-        int rear;
-        int *arr;
-    public:
-        Queue(int size){
-            this->size = size;
-            arr = new int[size];
-            front = -1;
-            rear = -1;
-        }
+class Queue {
+private:
+    int size;
+    int front;
+    int rear;
+    int *arr;
 
-        ~Queue(){
-            delete[] arr;
-        }
+public:
+    Queue(int size) {
+        this->size = size;
+        arr = new int[size];
+        front = -1;
+        rear = -1;
+    }
 
-        void Front(int element){
-            if(isFull()){
-                cout << "Queue is full" << endl;
-                return;
-            }
-            arr[front++] = element;
-        }
+    ~Queue() {
+        delete[] arr;
+    }
 
-        bool isEmpty() {
-            return front == -1;
-        }
+    bool isEmpty() {
+        return front == -1;
+    }
 
-        bool isFull() {
-            return (front == 0 && rear == size - 1) || (front == rear + 1);
+    bool isFull() {
+        return (front == 0 && rear == size - 1) || (front == rear + 1);
+    }
+    int getSize() {
+        return size;
+    }
+    int getFront() {
+        return front;
+    }
+    int getRear() {
+        return rear;
+    }
+    void Enqueue(int element) {
+        if (isFull()) {
+            cout << "Queue da day" << endl;
+            return;
         }
-
-        int Dequeue(){
-            if(isEmpty()){
-                return -1;
-            }
-            int data = arr[front];
-            if(front == rear){
-                front = rear = -1;
-            }else{
-                front = (front + 1) % size;
-            }
-            return data;
+        if (front == -1) {
+            front = 0;
         }
+        rear = (rear + 1) % size;
+        arr[rear] = element;
+    }
 
-        int Enqueue(){
-            if(isFull()){
-                return -1;
-            }
-            int data;
-            cin >> data;
-            if(front == -1){
-                front = 0;
-            }
-            rear = (rear + 1) % size;
-            arr[rear] = data;
+    int Dequeue() {
+        if (isEmpty()) {
+            cout << "Queue rong!" << endl;
+            return -1;
+        }
+        int data = arr[front];
+        if (front == rear) {
+            front = rear = -1;
+        } else {
+            front = (front + 1) % size;
+        }
+        return data;
+    }
+    
+    int updateSize() {
+        if (front == -1) {
             return 0;
         }
-
-        void print(){
-            if(isEmpty()){
-                cout << "Queue is empty" << endl;
-                return;
-            }
-            int i = front;
-            if(front <= rear){
-                while(i <= rear){
-                    cout << arr[i] << " ";
-                    i++;
-                }
-            }else{
-                while(i < size){
-                    cout << arr[i] << " ";
-                    i++;
-                }
-                i = 0;
-                while(i <= rear){
-                    cout << arr[i] << " ";
-                    i++;
-                }
-            }
-            cout << endl;
+        if (front <= rear) {
+            return rear - front + 1;
         }
+        return size - front + rear + 1;
+    }
+    void deleteElement(int index) {
+        if (isEmpty()) {
+            cout << "Queue rong!" << endl;
+            return;
+        }
+        if (index < 0 || index >= size) {
+            cout << "Vi tri khong hop le" << endl;
+            return;
+        }
+        if (front <= rear) {
+            for (int i = index; i < rear; i++) {
+                arr[i] = arr[i + 1];
+            }
+            rear--;
+        } else {
+            for (int i = index; i < size - 1; i++) {
+                arr[i] = arr[i + 1];
+            }
+            for (int i = 0; i < rear; i++) {
+                arr[i] = arr[i + 1];
+            }
+            rear--;
+        }
+    }
+    void print() {
+        if (isEmpty()) {
+            cout << "Queue is empty" << endl;
+            return;
+        }
+        int i = front;
+        if (front <= rear) {
+            while (i <= rear) {
+                cout << arr[i] << " ";
+                i++;
+            }
+        } else {
+            while (i < size) {
+                cout << arr[i] << " ";
+                i++;
+            }
+            i = 0;
+            while (i <= rear) {
+                cout << arr[i] << " ";
+                i++;
+            }
+        }
+        cout << endl;
+    }
 };
 
-int main(){
-    int n;
-    cin >> n;
-    Queue queue(n);
-    while(queue.isFull() == false){
-        int element;
-        cin >> element;
-        queue.Front(element);
-    }
-    queue.print();
+int main() {
+    int size;
+    cout << "Enter the size of the queue: ";
+    cin >> size;
+    Queue queue(size);
+
+    int choice;
+    do {
+        cout << "----- MENU -----" << endl;
+        cout << "1. Enqueue" << endl;
+        cout << "2. Dequeue" << endl;
+        cout << "3. Cap nhat size cua Queue" << endl;
+        cout << "4. Xoa phan tu cua queue" << endl;
+        cout << "5. Print Queue" << endl;
+        cout << "0. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1: {
+                int element;
+                cout << "Nhap phan tu vao Queue: ";
+                cin >> element;
+                queue.Enqueue(element);
+                break;
+            }
+            case 2: {
+                int dequeuedElement = queue.Dequeue();
+                if (dequeuedElement != -1) {
+                    cout << "Dequeued element: " << dequeuedElement << endl;
+                }
+                break;
+            }
+            case 3: {
+                int updatedSize = queue.updateSize();
+                cout << "Cap nhat size: " << updatedSize << endl;
+                break;
+            }
+            case 4: {
+                int index;
+                cout << "Nhap phan tu muon xoa: ";
+                cin >> index;
+                queue.deleteElement(index);
+                break;
+            }
+            case 5: {
+                cout << "Queue: ";
+                queue.print();
+                break;
+            }
+            case 0: {
+                cout  << endl;
+                break;
+            }
+            default: {
+                cout << "Lua chon khong hop le!" << endl;
+                break;
+            }
+        }
+    } while (choice != 0);
+
+    return 0;
 }
