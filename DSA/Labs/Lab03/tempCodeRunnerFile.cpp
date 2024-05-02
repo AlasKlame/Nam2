ed2b951eaa03145aef70decb3a99981dfaabf892
Vuco <vuledinhhoang@gmail.com>
2024-05-02 08:48:58 +0700

    update 2/5/2024

--- DSA/Labs/Lab03/tempCodeRunnerFile.cpp
+++ DSA/Labs/Lab03/tempCodeRunnerFile.cpp
@@ -1,2 +1,37 @@
 
-    if(pRoot == NULL){
+int main(){
+    Node *root = NULL;
+    int arr[] = {5, 3, 7, 2, 4, 6, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    root = createBST(arr, n);
+    
+    int x = 5;
+    int min = minNode(root);
+    cout << "Min of BST: " << min << endl;
+    int max = maxNode(root);
+    cout << "Max of BST: " << max << endl;
+
+    if(isBST(root, min, max)){
+        cout << "true" <<endl;
+    }
+    else{
+        cout << "false" << endl;
+    }
+    if(isFullBST(root)){
+        cout << "true" << endl;
+    }
+    else{
+        cout << "false" << endl;
+    }
+
+    int lessCount = countLess(root, x);
+    cout << "Number of nodes less than " << x << ": " << lessCount << endl;
+    
+    int greaterCount = countGreater(root, x);
+    cout << "Number of nodes greater than " << x << ": " << greaterCount << endl;
+    
+    removeBST(root);
+    
+    return 0;
+}
--- Test/tempCodeRunnerFile.cpp
+++ Test/tempCodeRunnerFile.cpp
@@ -1,1 +1,61 @@
-switch
+#include <iostream>
+using namespace std;
+
+// Hàm kiểm tra số lần xuất hiện của mỗi chữ số có lớn hơn k hay không
+bool countDigits(int n, int k) {
+    cout << "Số lần xuất hiện của mỗi chữ số trong số đã cho:" << endl;
+    for (int digit = 0; digit <= 9; digit++) {
+        int count = 0;
+        int temp = n;
+        while (temp > 0) {
+            if (temp % 10 == digit) {
+                count++;
+            }
+            temp /= 10;
+        }
+        if (count > k) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int createLargestSpecialNumber(int n, int k) {
+    int specialNumber = 0;
+    for (int digit = 9; digit >= 0; digit--) {
+        int temp = n;
+        int place = 1;
+        int nextPlace = 10;
+        while (temp > 0) {
+            int currentDigit = temp % 10;
+            // Chỉ thêm vào specialNumber những chữ số không vượt quá k lần
+            if (currentDigit == digit && k > 0) {
+                specialNumber += digit * place;
+                place = nextPlace;
+                nextPlace *= 10;
+                k--;
+            }
+            temp /= 10;
+        }
+    }
+    return specialNumber;
+}
+
+int main() {
+    int k, n;
+
+    cout << "Input k: ";
+    cin >> k;
+    cout << "Input n: ";
+    cin >> n;
+
+    if(countDigits(n, k)) {
+        cout << "Không cần xóa chữ số nào." << endl;
+        cout << "Output: " << n << endl;
+    } else {
+        int specialNumber = createLargestSpecialNumber(n, k);
+        cout << "Output: " << specialNumber << endl;
+    }
+
+    return 0;
+}
